dd6049a9039da50c11c5b02199453c8b411590a9
beniyam demissie <beni@beniyams-MacBook-Air.local>
2025-03-01 21:16:29 -0600

    updated about and contact page, added an SQL database

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import requests
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://flavor_quest_ofxa_user:dcCHNtDq9shCZaqjTGjLy2jX8NZ7LZfk@dpg-cv1scraj1k6c73975bcg-a.oregon-postgres.render.com/flavor_quest_ofxa"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'inebbeni'
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Contact {self.name}>'
+    
+
+
+
 
 
 API_KEY = "0329312fa1b943a49b245640c08a2872"
@@ -53,11 +77,24 @@ def about():
     
     return render_template("app_about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+
+        new_contact = Contact(name=name, email=email, message=message)
+        db.session.add(new_contact)
+        db.session.commit()
+
+        flash("Thank you for contacting us! We will get back to you soon.", "success")
+        
+        
+        return redirect(url_for('contact'))
     
     return render_template("app_contact.html")
 
 
-
-app.run(host="0.0.0.0", port=5001, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002, debug=False)
